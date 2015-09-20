cfdcb05ac8265e248b8892bfb97b6ced5283ed1b
Sal Sanci <salsanci@gmail.com>
2015-09-20 11:27:23 -0700

    Latest build

--- results/outputFiles/mpOptimize.f
+++ results/outputFiles/mpOptimize.f
@@ -1,3 +1,318 @@
+
+
+lockdict create thisL@ forthentry
+$C_a_lxasm w, h104  hFC  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h5CFD54A4 l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, h8BD9000 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create thisW@ forthentry
+$C_a_lxasm w, h104  hFC  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h5CFD54A4 l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, h4BD9000 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create thisC@ forthentry
+$C_a_lxasm w, h104  hFC  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h5CFD54A4 l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, hBD9000 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create padbl forthentry
+$C_a_lxasm w, h104  hFC  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BD97F0 l, h80FD9604 l, hA0FD9220 l, h83E06CB l, h80FD9604 l, hE4FD92FF l, h5C7C0073 l, h20202020 l, 
+
+freedict
+
+
+
+
+lockdict create key forthentry
+$C_a_lxasm w, h103  hFC  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD54A4 l, h4BD91F0 l, h627D9100 l, h5C5400FD l, hA0FC0100 l, h43C01F0 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create thisL! forthentry
+$C_a_lxasm w, h10D  h105  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, h83D9000 l, h5CFD72B3 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create thisW! forthentry
+$C_a_lxasm w, h10D  h105  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, h43D9000 l, h5CFD72B3 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create thisC! forthentry
+$C_a_lxasm w, h10D  h105  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, hA0BC04CB l, h4BC0002 l, h80FC0402 l, h80BC00E4 l, h3D9000 l, h5CFD72B3 l, h5C7C0075 l, 
+
+freedict
+
+
+
+
+lockdict create um* forthentry
+$C_a_lxasm w, h11D  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, hA0FC0200 l, hA0FD9200 l, hA0FD9400 l, h2BFD9001 l, h5C4C0116 l, h81BD9400 l, hC8BC02C9 l, 
+h2DFC0001 l, h34FD9201 l, h5C540112 l, hA0BD90CA l, h5CFD54A4 l, hA0BD9001 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create um/mod forthentry
+$C_a_lxasm w, h11E  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, hA0BD98C8 l, h5CFD72B3 l, hA0FD9240 l, hA0FC0200 l, h2DFD9001 l, h35FD9801 l, h35FC0201 l, 
+h84B00200 l, hE18C0200 l, h34FD9401 l, hE4FD9313 l, hA0BD9001 l, h5CFD54A4 l, hA0BD90CA l, h5C7C0073 l, 
+
+freedict
+
+
+
+
+lockdict create cstr= forthentry
+$C_a_lxasm w, h11A  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, hBC0200 l, h80FC0201 l, h84FD9001 l, hBD9200 l, h80FC0001 l, h80FD9001 l, hBD94C8 l, 
+h863D92CA l, hE4E80312 l, h78BD90C4 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create name= forthentry
+$C_a_lxasm w, h11F  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, hBC0200 l, h60FC021F l, h80FC0001 l, hBD94C8 l, h60FD941F l, hA0BD9201 l, h80FC0201 l, 
+h5C7C011B l, hBD9200 l, h80FC0001 l, h80FD9001 l, hBD94C8 l, h863D92CA l, hE4E80317 l, h78BD90C4 l, 
+h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create .str forthentry
+$C_a_lxasm w, h11D  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, h867C0000 l, hA09599F0 l, h80D59802 l, h69402CC l, h5C68011B l, hBD94C8 l, h80FD9001 l, 
+h4BD9201 l, h627D9300 l, h5C680116 l, h43D9401 l, hE4FC0114 l, h5CFD72B3 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create _fkey? forthentry
+$C_a_lxasm w, h118  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h4BC00C8 l, h627C0100 l, h78BC02C4 l, h5C540114 l, hA0FD9300 l, h43D92C8 l, hA0BD9000 l, h5CFD54A4 l, 
+hA0BD9001 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create fkey? forthentry
+$C_a_lxasm w, h118  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD54A4 l, h4BD91F0 l, h627D9100 l, h78BC00C4 l, h5C540115 l, hA0FD9300 l, h43D93F0 l, h5CFD54A4 l, 
+hA0BD9000 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create _femit? forthentry
+$C_a_lxasm w, h116  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, h60FD90FF l, h4BC0200 l, h627C0300 l, h7CBD92C4 l, h4159000 l, hA0BD90C9 l, h5C7C0073 l, 
+
+freedict
+
+
+
+
+lockdict create femit? forthentry
+$C_a_lxasm w, h11A  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h60FD90FF l, hA0BC03F0 l, h80FC0202 l, h6BD9201 l, h78BD94C4 l, h5C680118 l, h4BC02C9 l, h627C0300 l, 
+h7CBD94C4 l, h41590C9 l, hA0BD90CA l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create emit forthentry
+$C_a_lxasm w, h119  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, h60FC00FF l, hA0BC03F0 l, h80FC0202 l, h6BD9201 l, h5C680118 l, h4BC02C9 l, h627C0300 l, 
+h5C680114 l, h43C00C9 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create skipbl forthentry
+$C_a_lxasm w, h11F  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BC01F0 l, h80FC00DC l, h4BC0200 l, h877C0280 l, h5C4C011E l, hA0FD9280 l, h84BD9201 l, h80FC0204 l, 
+h80BC03F0 l, hBD9601 l, h867D9620 l, h80E80201 l, hE4E99317 l, h84FC0204 l, h84BC03F0 l, h43C0200 l, 
+h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create _qaf forthentry
+$C_a_lxasm w, h11E  h10E  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h8BC02C8 l, hA0BD9201 l, hA0BD9401 l, h60FC02FF l, h28FD9208 l, h60FD92FF l, h28FD9410 l, h60FD94FF l, 
+h87BC02C9 l, h80B002CA l, h84BD9401 l, h84FD9401 l, hA0BD9001 l, h5CFD54A4 l, hA0BD90CA l, h5C7C0073 l, 
+
+freedict
+
+
+
+
+lockdict create _eeread forthentry
+$C_a_lxasm w, h13F  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5C7C0127 l, h20000000 l, h10000000 l, hD l, hA0BD9923 l, hE4FD9925 l, h5C7C0000 l, hA0BC00C8 l, 
+hA0FD9000 l, h64BFED21 l, hA0FD9208 l, h5CFE4D24 l, h613E43F2 l, h34FD9001 l, h68BFE922 l, h5CFE4D24 l, 
+h5CFE4D24 l, h64BFE922 l, h5CFE4D24 l, hE4FD932B l, h867C0000 l, h7CBFE921 l, h68BFED21 l, h5CFE4D24 l, 
+h68BFE922 l, h5CFE4D24 l, h5CFE4D24 l, h64BFE922 l, h64BFE921 l, h5CFE4D24 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create _dictsearch forthentry
+$C_a_lxasm w, h138  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD72B2 l, hA0BD9600 l, hA0BD98C8 l, hBC0200 l, h60FC021F l, h80FC0001 l, hBD94C8 l, h60FD941F l, 
+hA0BD9201 l, h80FC0201 l, h5C7C012F l, hBD9200 l, h80FC0001 l, h80FD9001 l, hBD94C8 l, h863D92CA l, 
+hE4E8032B l, hA0BD90CC l, h5C680073 l, h84FD9802 l, h6BD90CC l, h5C680073 l, hA0BC00CB l, h5C7C0122 l, 
+
+freedict
+
+
+
+
+lockdict create _eewrite forthentry
+$C_a_lxasm w, h13E  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5C7C0127 l, h20000000 l, h10000000 l, hD l, hA0BD9923 l, hE4FD9925 l, h5C7C0000 l, hA0FD9208 l, 
+h627D9080 l, h7CBFE921 l, h5CFE4D24 l, h68BFE922 l, h5CFE4D24 l, h5CFE4D24 l, h64BFE922 l, h2CFD9001 l, 
+h5CFE4D24 l, hE4FD9328 l, h64BFED21 l, h623E43F2 l, h7CBD90C4 l, h5CFE4D24 l, h68BFE922 l, h5CFE4D24 l, 
+h5CFE4D24 l, h64BFE922 l, h5CFE4D24 l, h64BFE921 l, h68BFED21 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create _accept forthentry
+$C_a_lxasm w, h14D  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD54A4 l, hA0BC03F0 l, h80FC0202 l, h4BD9801 l, h80FC02DD l, hBC0001 l, h627C0008 l, hA0D59800 l, 
+h84FC02DA l, hA0BD9001 l, hA0FD927E l, h4BD95F0 l, h627D9500 l, h5C54012B l, hA0FC0100 l, h43C01F0 l, 
+h867D940D l, hA0E99201 l, h5C680142 l, h867D9408 l, h5C680139 l, h48FD9420 l, h3D94C8 l, h80FD9001 l, 
+h5C7C0142 l, h5CFE9946 l, hA0FD9420 l, h5CFE9946 l, h84FD9001 l, h48BD9001 l, h3D94C8 l, h80FD9202 l, 
+h4CFD927E l, hA0FD9408 l, h5CFE9946 l, hE4FD932B l, h84BD9001 l, h5C7C0073 l, h867D9800 l, h5C68014C l, 
+h4BC00CC l, h627C0100 l, h5C680148 l, h43D94CC l, h5C7C0000 l, 
+freedict
+
+
+
+
+lockdict create _toq forthentry
+$C_a_lxasm w, h143  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BD98C8 l, h5CFD72B3 l, h5CFE8524 l, h5C7C0073 l, h8BC00CC l, hA0BC0200 l, h60FC02FF l, hA0BD9200 l, 
+h28FD9208 l, h60FD92FF l, hA0BD9400 l, h28FD9410 l, h60FD94FF l, hA0BD9601 l, h80FD9601 l, h873D96CA l, 
+hA0CD9600 l, h863D96C9 l, hA0E99000 l, h5C680142 l, hA0BD9400 l, h28FD9418 l, h60FD9403 l, h2CBC02CA l, 
+h80FC0204 l, h80BC02CC l, h627D9403 l, h299001 l, h627D9401 l, h4159001 l, h627D9402 l, h8159001 l, 
+hA4FD9001 l, h3D96CC l, h5C7C0000 l, 
+freedict
+
+
+
+
+lockdict create _frq forthentry
+$C_a_lxasm w, h146  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BD98C8 l, h5CFE8B25 l, h5CFD54A4 l, hA0BD90CB l, h5C7C0073 l, h8BC00CC l, hA0BC0200 l, h60FC02FF l, 
+hA0BD9200 l, h28FD9208 l, h60FD92FF l, h863C02C9 l, hA0E99600 l, h5C680145 l, hA0BD9400 l, h28FD9418 l, 
+h60FD9403 l, hA0BD96C9 l, h2CBD92CA l, h80FD9204 l, h80BD92CC l, h627D9403 l, hA990C9 l, h627D9401 l, 
+h49590C9 l, h627D9402 l, h89590C9 l, hA0BD9400 l, h28FD9410 l, h60FD94FF l, h80FD9601 l, h873D96CA l, 
+hA0CD9600 l, h80FD9801 l, h3D96CC l, h84FD9801 l, hA4FD9601 l, h5C7C0000 l, 
+freedict
+
+
+
+
+lockdict create slice forthentry
+$C_a_lxasm w, h169  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+h5CFD80BA l, h867DC800 l, h5C680136 l, hBC00E4 l, h80FDC801 l, h863C00C6 l, hBC00E4 l, hA0D59805 l, 
+h5C5400A0 l, h863C00C7 l, hA0D59806 l, h5C5400A0 l, h80FDC803 l, h8BC00E4 l, h80FDC804 l, hA0BD93F1 l, 
+h8BC02E4 l, h84BD9200 l, hA8BD94C9 l, h48BC02CA l, h83C02E4 l, h84FDC808 l, h60BDC8C3 l, h2CFDC810 l, 
+h68BD96E4 l, hD7ECE06 l, h5C700139 l, h8BC0168 l, hA0BC0200 l, h60FC02FF l, hA0BD9200 l, h28FD9208 l, 
+h60FD92FF l, hA0BD9400 l, h28FD9410 l, h60FD94FF l, hA0BC0001 l, h2CFC0002 l, h80FC0004 l, h80BC0168 l, 
+h83D9600 l, hA0BC00C9 l, h2CFC0002 l, h80FC0004 l, h80BC0168 l, h80FC0201 l, h80FD9201 l, h8BC0400 l, 
+h873C02CA l, hA0CC0200 l, h873D92CA l, hA0CD9200 l, h2CFD9208 l, h68BC02C9 l, h43C0368 l, hA0BDC802 l, 
+h60BC04C3 l, h2AFDC810 l, hC7ECE07 l, h5C680166 l, hA0BC01F0 l, h80FC00CE l, h43DC800 l, h3D8CE4 l, 
+h80FDC801 l, h3D8EE4 l, h80FDC803 l, hA0BC01F1 l, h83C00E4 l, h84FDC804 l, h5C7C0075 l, h7 l, 
+h7DD4 l, 
+freedict
+
+
+
+
+lockdict create gc1 forthentry
+$C_a_lxasm w, h143  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hD7E8206 l, h5C700120 l, h8BC0142 l, hA0BC0200 l, h60FC02FF l, hA0BD9200 l, h28FD9208 l, h60FD92FF l, 
+hA0BD9400 l, h28FD9410 l, h60FD94FF l, hA0BC00C9 l, h2CFC0001 l, h80FC0004 l, h80BC0142 l, h4BD9600 l, 
+h2BC00CB l, hA0BC0001 l, h2CFC0001 l, h80FC0004 l, h80BC0142 l, h80FD9201 l, h80D40201 l, h4159600 l, 
+h873C02CA l, hA0CC0200 l, h873D92CA l, hA0CD9200 l, h2CFD9208 l, h68BC02C9 l, h43C0342 l, hC7E8207 l, 
+h5C7C0073 l, h7 l, h7CBC l, 
+freedict
+
+
+
+
+lockdict create xisnumber forthentry
+$C_a_lxasm w, h14D  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BD92C8 l, h5CFD72B3 l, hA0BD94C8 l, hA0BD90C4 l, hA0BC01F0 l, h80FC00D2 l, hA0FC0200 l, hBD96CA l, 
+h867D967A l, hA0E80240 l, h867D9668 l, hA0E80210 l, h867D9664 l, hA0E8020A l, h867D9662 l, hA0E80202 l, 
+h867C0200 l, h4A80200 l, h80D59401 l, h84D59201 l, hBD96CA l, h867D962D l, h80E99401 l, h84E99201 l, 
+hBD96CA l, h80FD9401 l, h867D965F l, h5E68014B l, h84FD9630 l, hC37D9609 l, h5C780142 l, h84FD9607 l, 
+hC37D960A l, hA0B196C4 l, hC37D9626 l, h5C780147 l, h84FD9603 l, hC37D9627 l, hA0B196C4 l, h873D9601 l, 
+hA0B196C4 l, hA0CD9600 l, h62BD90CB l, hE4D59338 l, h5C7C0073 l, 
+freedict
+
+
+
+
+lockdict create xnumber forthentry
+$C_a_lxasm w, h153  h120  1- tuck - h9 lshift swap h1FF and or here W@ alignl h10 lshift or l,
+hA0BD92C8 l, h5CFD72B3 l, hA0BD94C8 l, hA0FD9000 l, hA0BC01F0 l, h80FC00D2 l, hA0FC0200 l, hBD96CA l, 
+h867D967A l, hA0E80240 l, h867D9668 l, hA0E80210 l, h867D9664 l, hA0E8020A l, h867D9662 l, hA0E80202 l, 
+h867C0200 l, h4A80200 l, h80D59401 l, h84D59201 l, hBD96CA l, h867D962D l, h78BD98C4 l, h80E99401 l, 
+h84E99201 l, hBD96CA l, h80FD9401 l, h867D965F l, h5C68014F l, h84FD9630 l, hC37D9609 l, h5C780143 l, 
+h84FD9607 l, hC37D960A l, hA0B196C4 l, hC37D9626 l, h5C780148 l, h84FD9603 l, hC37D9627 l, hA0B196C4 l, 
+hA0BC00C8 l, hA0BD90CB l, hA0BD9601 l, h2BFD9601 l, h80B19000 l, h2CFC0001 l, h5C54014B l, hE4FD9339 l, 
+h867D9800 l, hA49590C8 l, h5C7C0073 l, 
+freedict
+
+
+: init_coghere h169 coghere W! ;
+h11F wconstant build_BootOpt
+h10D wconstant build_BootOpt1
+h104 wconstant build_BootOpt2
+hFB wconstant build_BootOpt3
+
 \
 
 \ Output of assembling OpimizeAsmSrc.f goes above this comment block
