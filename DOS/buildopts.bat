306616023070ed3230630132ee54791984a8e3eb
Sal Sanci <salsanci@gmail.com>
2015-10-12 13:02:00 -0700

    Built needs verification

--- DOS/buildopts.bat
+++ DOS/buildopts.bat
@@ -4,7 +4,7 @@ if %INGOSHELL% neq 1 set domakerr=999
 if %domakeerr% neq 0 goto end
 
 cd ..
-DOS\propellent /PORT %PROPCOMM% /EEPROM results/outputFiles/StartKernel.eeprom
+DOS\propellent /PORT %PROPCOMM% /EEPROM results\outputFiles\StartKernel.eeprom
 if %ERRORLEVEL% neq 0 set domakeerr=998
 if %domakeerr% neq 0 goto end
 
--- DOS/test.bat
+++ DOS/test.bat
@@ -3,6 +3,12 @@ set PROPBAUD=230400
 set PROPFLOWCONTROL=1
 set INGOSHELL=1
 set PATH=%PATH%;tools\mygo\bin
-call buildStartKernel.bat
-echo buildStartKernel.bat result: %domakeerr%
+
+call mp\buildMpKernel.bat
+echo buildMpKernel.bat result: %domakeerr%
 if %domakeerr% neq 0 goto end
+
+call mp\buildMpKernelSpin.bat
+echo buildMpKernelSpin.bat result: %domakeerr%
+if %domakeerr% neq 0 goto end
+
